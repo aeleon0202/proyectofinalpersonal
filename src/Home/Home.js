8ea88b15bc0ad54e1c500d43680ffc856844dd97
407 <407@academicos.cesde.edu.co>
2022-11-29 16:38:59 -0500

    se crea proyecto de consumo de API

---
+++ src/Home/Home.js
@@ -0,0 +1,78 @@
+import './Home.css'
+export function Home(){
+
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8 mt-5">
+                            <h2>LINKIN PARK</h2>
+                            <p>Linkin Park es una banda estadounidense de rock alternativo procedente de Agoura Hills, California formada en 1996. Integrada por Mike Shinoda, Dave Farrell, Joe Hahn, Brad Delson, Rob Bourdon y Chester Bennington, este último como voz principal.</p>
+                            <img src='https://firebasestorage.googleapis.com/v0/b/spotyanderl.appspot.com/o/linkin%202.jpg?alt=media&token=9d4ff683-4216-49a5-b921-2a2cd6f6333b' class="mt-5"></img>
+                        </div>
+                        <div class="col-12 col-md-4 bg-secondary mt-5">
+                            <div>
+                                <div class="columna">
+         
+                                        <i class="bi bi-vinyl-fill fs-1"></i>
+                                        <h3>MEDELLIN</h3>
+                                        <h4>DIC 12</h4>
+                                    
+                                </div>
+                                
+                                <div class="columna">
+                                    <i class="bi bi-vinyl-fill fs-1"></i>
+                                </div>
+                                <div class="columna">
+                                    <h3>BOGOTA</h3>
+                                    <h4>DIC 13</h4>
+                                </div>
+                                <div>
+                                    <i class="bi bi-vinyl-fill fs-1"></i>
+                                </div>
+                                <div>
+                                    <h3>CALI</h3>
+                                    <h4>DIC 14</h4>
+                                </div>
+                                <div>
+                                    <i class="bi bi-vinyl-fill fs-1"></i>
+                                </div>
+                                <div>
+                                    <h3>PEREIRA</h3>
+                                    <h4>DIC 15</h4>
+                                </div>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="container-fluid mt-5">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyanderl.appspot.com/o/muerte%20chester.jpg?alt=media&token=34c6ab73-4172-4baa-8689-2809ba125d9a" alt="" class="img-fluid w-100"/>
+                            
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyanderl.appspot.com/o/concierto.jpg?alt=media&token=8030f76c-a366-461a-9826-1811f3fae29b" alt="" class="img-fluid w-100"/>
+                            
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyanderl.appspot.com/o/lanzamiento.jpg?alt=media&token=03e313b3-f7b4-4d85-811e-c92c34dfceec" alt="" class="img-fluid w-100"/>
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
+}
